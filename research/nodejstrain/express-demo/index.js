2eb9a104ac95d01a806619b98e3c51d07cdad2ea
dexterchan <dexterchan@gmail.com>
2018-11-25 22:43:57 +0800

    middleware tutorial

--- research/nodejstrain/express-demo/index.js
+++ research/nodejstrain/express-demo/index.js
@@ -1,104 +1,53 @@
 const express=require("express");
+
+
 const app=express();
-const Joi = require('joi');
-const logger = require("./logger");
+
+const Helmet=require('helmet');
+const Morgan = require('morgan');
+const customlogger = require("./middleware/logger");
+const config = require("config");
+
+const courses_router=require("./routes/courses");
+const rootpage_router=require("./routes/root");
+
+const StartDebugger = require("debug")("app:start");
+const DBDebugger = require("debug")("app:db");
+
+console.log(`NODE_ENV:${process.env.NODE_ENV}`);
+console.log(`NODE_ENV: ${app.get("env")}`);
+
+app.set ("view engine","pug");
+app.set ("views","./views");
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));//key1=value1&key2=value2
 app.use(express.static("public")); //static page
+app.use("/api/courses",courses_router);
+app.use("/",rootpage_router);
+
+
+//Configuration
+console.log("Application Name:" + config.get("name"));
+console.log("mail server:" + config.get("mail.host"));
+console.log("mail password:" + config.get("mail.password"));
+
 //custom middleware
-app.use( logger);
-
-const courses = [
-    {id:1, name:"course1"},
-    {id:2, name:"course2"},
-    {id:3, name:"course3"}
-];
-
-app.get("/",(req,res)=>{
-    res.send("Hello World!!!")
-});
-
-app.get("/api/courses", (req,res)=>{
-    res.send(courses);
-});
-
-app.post("/api/courses", (req,res)=>{
-    
-    const checkResult=validateCourse(req.body);
-    if(checkResult.error != null) return res.status(400).send(checkResult.error.details[0].message);//400 Bad Request
-        
-/*
-    if(!req.body.name || req.body.name.length<3){
-        //400 Bad Request
-        res.status(400).send("name is required and should be minimum 3 characters");
-        return;
-    }*/
-    const course = {
-        id: courses.length+1,
-        name:req.body.name
-    };
-    courses.push(course);
-    res.send(course);
-});
-
-app.put("/api/courses/:id", (req,res)=>{
-    //Lookup the course
-    //If not exist, return 404
-    let course=courses.find(c=> c.id === parseInt(req.params.id) );
-    if(!course) return res.status(404).send("The course with the given id not found");
-
-    //Validate 
-    
-    //const checkResult=validateCourse(req.body);
-    const {error}=validateCourse(req.body);//Object destructuring
-    //if invalidate, return 400 - Bad Request
-    if(error!= null) return res.status(400).send(error.details[0].message); //400 Bad Request
-
-    //Update course
-    course.name=req.body.name;
-
-    res.send(course); 
-});
-
-app.delete("/api/courses/:id",(req,res)=>{
-    //Lookup the course
-    //If not exist, return 404
-    let course=courses.find(c=> c.id === parseInt(req.params.id) );
-    if(!course) return res.status(404).send("The course with the given id not found");
-
-    //delete
-    const index = courses.indexOf(course);
-    courses.splice(index,1);
-    
-    res.send(course);
-
-});
-function validateCourse(course){
-    const schema={
-        name:Joi.string().min(3).required()
-    };
-    return Joi.validate(course,schema);
-
+//app.use( customlogger);
+if (process.env.NODE_ENV === "production"){
+    app.use(Helmet());
+    console.log("Production loading Helmet...");
+    app.use(Morgan("common"));
+    console.log("Production writing log");
+}else{
+    app.use(customlogger);
+    console.log("Development writing simple log");
 }
 
-// /api/courses/1
-app.get("/api/courses/:id",(req,res)=>{
-    //"let" allows u to re-assign var once more time
-    let course=courses.find(c=> c.id === parseInt(req.params.id) );
-    if(!course) return res.status(404).send("The course with the given id not found");
-    res.send(course);
-}
-);
-
-// /api/posts/year/month
-app.get("/api/posts/:year/:month",(req,res)=>{
-    //res.send ( req.params ); //send parameters year, month
-    res.send(req.query); //send query ?abc=def
-}
-);
+DBDebugger("DB connected");
 
 
 //PORT
 const port=process.env.PORT || 3000
-
+StartDebugger("Started app");
 app.listen(port,()=>console.log(`Listening on port: ${port}...`));
--- research/nodejstrain/express-demo/middleware/logger.js
+++ research/nodejstrain/express-demo/middleware/logger.js
@@ -2,8 +2,6 @@
 const logfunc=(req,res,next)=>{
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`Logging.... ${req.method} ${req.path} from ${ip}`);
-    
-
     next();
 };
 
