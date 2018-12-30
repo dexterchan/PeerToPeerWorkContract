0f6b7df6ce04ef78c63770feb4d0ac1e2711321d
dexterchan <dexterchan@gmail.com>
2018-12-30 23:27:44 +0800

    sync FInance service URI

--- PeerToPeerFrontEnd/config/SystemSetting.js
+++ PeerToPeerFrontEnd/config/SystemSetting.js
@@ -3,7 +3,10 @@ const debug = require("debug")("app:DEBUG");
 
 const SYS={
     ecashorder_url:{
-        default:"http://localhost:8001/api/ecashorder"
+        default:"http://localhost:8001/api/ecashorder",
+        development:"http://localhost:8001/api/ecashorder",
+        SIT:"http://192.168.1.251:8001/api/ecashorder",
+        UAT:"http://dexcloudapp.xyz:8888/api/ecashorder"
     }
 }
 
@@ -12,10 +15,14 @@ module.exports=(key)=>{
     let retValue;
     if(SYS[key]){
         let mode;
-        if(process.env.NODE_ENV !== 'production'){
+        
+        mode = process.env.RUN_ENV;
+        if(mode.length==0){
             mode="default";
         }
-        debug(`System config Running in mode: ${mode}`)
+        
+        debug(`System config Running in mode: ${mode} ${SYS[key][mode]}`)
+       
         return SYS[key][mode];
     }
     return retValue;
--- README.md
+++ README.md
@@ -8,7 +8,13 @@
 docker build -f Dockerfile.PeerToPeerFrontEnd --tag p2pworkcontract/peer2peerfrontend .
 
 ### Docker run
-docker run -it -e PORT=8080 --rm -p 8080:8080 p2pworkcontract/peer2peerfrontend
+ENV variable
+RUN_DEV - point to finance service URI 
+default -> http://localhost:8001/api/ecashorder
+SIT -> http://192.168.1.251:8001/api/ecashorder
+UAT -> http://dexcloudapp.xyz:8001/api/ecashorder
+
+docker run -it -e PORT=8080 -e RUN_DEV=UAT --rm -p 8080:8080 p2pworkcontract/peer2peerfrontend
 
 #Finance Service
 ## Manage eCashOrder creation, asymmetric encryption, verification of digital signature
