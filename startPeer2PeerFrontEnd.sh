74de62b28abf3105d9164a4962995390d85f1d05
dexterchan <dexterchan@gmail.com>
2018-12-30 23:35:25 +0800

    fix

--- startPeer2PeerFrontEnd.sh
+++ startPeer2PeerFrontEnd.sh
@@ -1,1 +1,1 @@
-docker run -it -e PORT=8080 -e RUN_DEV=UAT --rm -p 8080:8080 p2pworkcontract/peer2peerfrontend
+docker run -it -e PORT=8080 -e RUN_ENV=UAT --rm -p 8080:8080 p2pworkcontract/peer2peerfrontend
