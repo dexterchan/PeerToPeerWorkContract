b88c424a0a6df653497a45868186a765f685a9ba
dexterchan <dexterchan@gmail.com>
2019-01-06 21:56:41 +0800

    docker

--- gke.step2.buildDockerImage.sh
+++ gke.step2.buildDockerImage.sh
@@ -10,7 +10,11 @@ DOCKER_IMAGE_NAME=gcr.io/$PROJECT_ID/$CLUSTERNAME-$SERVICE:v1
 docker build -f Dockerfile.financeservice --tag $DOCKER_IMAGE_NAME .
 
 #cp Dockerfile.financeservice Dockerfile
-
 #gcloud  builds submit  --tag $DOCKER_IMAGE_NAME .
+gcloud docker -- push $DOCKER_IMAGE_NAME
 
+export SERVICE=peer2peerfrontend
+DOCKER_IMAGE_NAME=gcr.io/$PROJECT_ID/$CLUSTERNAME-$SERVICE:v1
+
+docker build -f Dockerfile.PeerToPeerFrontEnd --tag $DOCKER_IMAGE_NAME .
 gcloud docker -- push $DOCKER_IMAGE_NAME
--- gke.step3.deployPod.sh
+++ gke.step3.deployPod.sh
@@ -3,16 +3,18 @@
 export PROJECT_ID=mlpractise
 export CLUSTERNAME=peer2peerworkcontract
 export ZONE=us-central1-c
-
-
 gcloud container clusters get-credentials $CLUSTERNAME --zone $ZONE --project $PROJECT_ID
 kubectl delete deployment financeservice
 read -p "Press [Enter] key to start deployment..."
 kubectl create -f ./containerDeploy/gke/financeService.deploy.yaml
 
+kubectl create -f ./containerDeploy/gke/peertopeerfrontend.deploy.yaml
+
 kubectl get deployments
 
-kubectl expose deployment financeservice --target-port=8888 --type=NodePort
-#read -p "Press [Enter] key to continue to create service..."
-#kubectl create -f $ASSET-lcm-service.yaml
-#kubectl get services
+
+#kubectl expose deployment financeservice --target-port=8888 --type=NodePort
+kubectl delete service peer2peercontract
+read -p "Press [Enter] key to continue to create service..."
+kubectl create -f ./containerDeploy/gke/LoadBalance.yaml
+kubectl get services
