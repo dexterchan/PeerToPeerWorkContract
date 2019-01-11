2e15e3238f3d73ec32c6b474f3cde435821d20b0
dexterchan <dexterchan@gmail.com>
2019-01-11 22:10:34 +0800

    gcp

--- containerDeploy/gke/runContainerDeploy.sh
+++ containerDeploy/gke/runContainerDeploy.sh
@@ -2,4 +2,5 @@ kubectl create -f deployment.yaml
 
 kubectl create -f Service.yaml
 
-kubectl apply -f fanout-ingress.yaml
+read -p "Press [Enter] key to start ingress deployment..."
+kubectl apply -f fanout.ingress.yaml
