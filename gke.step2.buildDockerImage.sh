3dd80fdf72d7b89b999678b1d6c1d77aaf42ed5c
dexterchan <dexterchan@gmail.com>
2019-01-11 15:38:51 +0800

    fix

--- gke.step2.buildDockerImage.sh
+++ gke.step2.buildDockerImage.sh
@@ -17,8 +17,8 @@ gcloud  builds submit  --tag $DOCKER_IMAGE_NAME .
 export SERVICE=peer2peerfrontend
 DOCKER_IMAGE_NAME=gcr.io/$PROJECT_ID/$CLUSTERNAME-$SERVICE:v3
 
-#docker build -f Dockerfile.PeerToPeerFrontEnd --tag $DOCKER_IMAGE_NAME .
-#gcloud docker -- push $DOCKER_IMAGE_NAME
+docker build -f Dockerfile.PeerToPeerFrontEnd --tag $DOCKER_IMAGE_NAME .
+gcloud docker -- push $DOCKER_IMAGE_NAME
 
-cp Dockerfile.financeservice Dockerfile
-gcloud  builds submit  --tag $DOCKER_IMAGE_NAME .
+#cp Dockerfile.financeservice Dockerfile
+#gcloud  builds submit  --tag $DOCKER_IMAGE_NAME .
