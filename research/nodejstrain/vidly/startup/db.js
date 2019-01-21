b0761302c4ef3059a1ca2a20cd3bb7f2c24aaef9
dexterchan <dexterchan@gmail.com>
2019-01-21 20:05:03 +0800

    vidly on heroku

--- research/nodejstrain/vidly/startup/db.js
+++ research/nodejstrain/vidly/startup/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const {logger} = require("./logging");
+const config = require("config");
 module.exports=()=>{
-    mongoose.connect('mongodb://localhost/vidly')
-  .then(() => logger.info("Connected to MongoDB...."));
+    const conn_str = config.get("db");
+    mongoose.connect(conn_str)
+        .then(() => logger.info("Connected to MongoDB...."));
   /*
   .catch(err => {
       logger.error('Could not connect to MongoDB...');
--- research/nodejstrain/vidly/startup/prod.js
+++ research/nodejstrain/vidly/startup/prod.js
@@ -1,7 +1,7 @@
 const helmet = require("helmet");
 const compression = require("compression");
 
-module.export= (app) =>{
+module.exports= (app) =>{
     app.use(helmet());
     app.use(compression());
 };
