0ebb3f0c3a4fd1539bb52339748e00cfb055a500
dexterchan <dexterchan@gmail.com>
2019-01-27 12:49:37 +0800

    change new contract for demo

--- PeerToPeerFrontEnd/ethereum/factory.js
+++ PeerToPeerFrontEnd/ethereum/factory.js
@@ -3,7 +3,7 @@ import web3 from './web3_query.js';
 const compiledFactoryABI=require("./buildV5/Peer2PeerProjectDashBoard.abi.json");
 
 
-const deployedAddress="0xb2e9617eeC43e176A6325D70c13Edb267089A1b7";
+const deployedAddress="0xD7f2DA11867f6D9796abDEc271C3E607444112B1";
 
 
 export default new web3.eth.Contract(compiledFactoryABI,deployedAddress);
