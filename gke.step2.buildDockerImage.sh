f275ed48db206bf1f35fc826e3e29105594bd5ec
dexterchan <dexterchan@gmail.com>
2019-01-29 21:58:35 +0800

    fix default config of finance url

--- gke.step2.buildDockerImage.sh
+++ gke.step2.buildDockerImage.sh
@@ -15,7 +15,7 @@ gcloud docker -- push $DOCKER_IMAGE_NAME
 
 
 export SERVICE=peer2peerfrontend
-DOCKER_IMAGE_NAME=gcr.io/$PROJECT_ID/$CLUSTERNAME-$SERVICE:v3.2
+DOCKER_IMAGE_NAME=gcr.io/$PROJECT_ID/$CLUSTERNAME-$SERVICE:v3.3
 
 docker build -f Dockerfile.PeerToPeerFrontEnd --tag $DOCKER_IMAGE_NAME .
 gcloud docker -- push $DOCKER_IMAGE_NAME
