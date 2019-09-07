ca5072d8b105bb3efdab23fb796fb16973adccec
dexterchan <dexterchan@gmail.com>
2019-09-07 19:01:48 +0800

    fix url

--- PeerToPeerFrontEnd/config/SystemSetting.js
+++ PeerToPeerFrontEnd/config/SystemSetting.js
@@ -1,31 +1,28 @@
-
 const debug = require("debug")("app:DEBUG");
 
-const SYS={
-    ecashorder_url:{
-        default:"http://apiapp.xyz/api/ecashorder",
-        development:"http://localhost:8001/api/ecashorder",
-        SIT:"http://192.168.1.251:8001/api/ecashorder",
-        UAT:"http://apiapp.xyz:8888/api/ecashorder",
-        K8:"http://apiapp.xyz/api/ecashorder"
-    }
-}
+const SYS = {
+  ecashorder_url: {
+    default: "http://apiapp.xyz/api/ecashorder/",
+    development: "http://localhost:8001/api/ecashorder/",
+    SIT: "http://192.168.1.251:8001/api/ecashorder/",
+    UAT: "http://apiapp.xyz:8888/api/ecashorder/",
+    K8: "http://apiapp.xyz/api/ecashorder/"
+  }
+};
 
+module.exports = key => {
+  let retValue;
+  if (SYS[key]) {
+    let mode;
 
-module.exports=(key)=>{
-    let retValue;
-    if(SYS[key]){
-        let mode;
-        
-        mode = process.env.RUN_ENV;
-        debug(`System config plan to run in mode: ${mode} ${SYS[key][mode]}`)
-        if(mode==undefined || mode.length==0){
-            mode="default";
-        }
-        debug(`System config Running in mode: ${mode} ${SYS[key][mode]}`)
-        
-       
-        return SYS[key][mode];
+    mode = process.env.RUN_ENV;
+    debug(`System config plan to run in mode: ${mode} ${SYS[key][mode]}`);
+    if (mode == undefined || mode.length == 0) {
+      mode = "default";
     }
-    return retValue;
-}
+    debug(`System config Running in mode: ${mode} ${SYS[key][mode]}`);
+
+    return SYS[key][mode];
+  }
+  return retValue;
+};
--- gke.step2.buildDockerImage.sh
+++ gke.step2.buildDockerImage.sh
@@ -6,7 +6,8 @@ export SERVICE=financeservice
 DOCKER_IMAGE_NAME=gcr.io/$PROJECT_ID/$CLUSTERNAME-$SERVICE:v4.1
 
 docker build -f Dockerfile.financeservice --tag $DOCKER_IMAGE_NAME .
-gcloud docker -- push $DOCKER_IMAGE_NAME
+docker push $DOCKER_IMAGE_NAME
+#gcloud docker -- push $DOCKER_IMAGE_NAME
 #cp Dockerfile.financeservice Dockerfile
 #gcloud  builds submit  --tag $DOCKER_IMAGE_NAME .
 
@@ -15,7 +16,8 @@ export SERVICE=peer2peerfrontend
 DOCKER_IMAGE_NAME=gcr.io/$PROJECT_ID/$CLUSTERNAME-$SERVICE:v4.3
 
 docker build -f Dockerfile.PeerToPeerFrontEnd --tag $DOCKER_IMAGE_NAME .
-gcloud docker -- push $DOCKER_IMAGE_NAME
+docker push $DOCKER_IMAGE_NAME
+#gcloud docker -- push $DOCKER_IMAGE_NAME
 
 
 export SERVICE=customerservice
